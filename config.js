848c46e1238b6da237cc79cbb83abf9fd9d350fa
Tony <tomcatbuzz@yahoo.com>
2021-03-22 13:06:14 -0400

    initial-working

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCqjFaTMwkLbSTGdC_Q4KQE-qprL6tyJNo",
+  authDomain: "fitness247-1a962.firebaseapp.com",
+  projectId: "fitness247-1a962",
+  storageBucket: "fitness247-1a962.appspot.com",
+  messagingSenderId: "610056733465",
+  appId: "1:610056733465:web:f3afbfda57e8e62d71fb88",
+  measurementId: "G-TK4XNYRP6B"
+}
+// Exports
+module.exports = firebaseConfig;
